f65b5752334d836d61c02fffaed6c62665654334
kusumoto <kusumoto@PC10312.local>
2018-11-18 15:42:17 +0900

    Initial Commit

---
+++ ClassScheduleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassScheduleApp
+//
+//  Created by kusumoto on 2018/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
